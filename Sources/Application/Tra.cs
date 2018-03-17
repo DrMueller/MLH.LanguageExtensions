8d6f22322cbecfc13bcc89f2a7fbc726b4c46c26
Matthias Mueller <matthiasm@live.de>
2018-03-17 20:57:12 +0100

    WIP

---
+++ Sources/Application/Tra.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mmu.Mlh.LanguageExtensions
+{
+    public class Tra
+    {
+        public static void Tra2(string t)
+        {
+            
+        }
+    }
+}
