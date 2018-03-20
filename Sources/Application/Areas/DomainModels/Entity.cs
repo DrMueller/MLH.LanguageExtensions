7960cead0f41a05b8cc4a74d6e45bee1847486e0
Matthias Mueller <matthiasm@live.de>
2018-03-20 13:20:44 +0100

    WIP

--- Sources/Application/Areas/DomainModels/Entity.cs
+++ Sources/Application/Areas/DomainModels/Entity.cs
@@ -1,12 +1,58 @@
-﻿using Mmu.Mlh.LanguageExtensions.Areas.DomainModels.Abstractions;
+﻿using System;
+using Mmu.Mlh.LanguageExtensions.Areas.DomainModels.Abstractions;
 
 namespace Mmu.Mlh.LanguageExtensions.Areas.DomainModels
 {
-    public abstract class Entity : ComparableIdentityProvider, IIdentityProvider
+    public abstract class Entity : IIdentityProvider
     {
-        protected Entity(string id) => Id = id;
+        protected Entity(string id)
+        {
+            Id = id;
+            IsTransient = true; // If we're in this constructor, it's a new Entity
+        }
 
         public string Id { get; private set; }
-        protected override string ComparedId => Id;
+        public bool IsTransient { get; }
+
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as Entity;
+
+            if (ReferenceEquals(compareTo, null))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, compareTo))
+            {
+                return true;
+            }
+
+            if (GetType() != compareTo.GetType())
+            {
+                return false;
+            }
+
+            return !IsTransient && !compareTo.IsTransient && Id == compareTo.Id;
+        }
+
+        public override int GetHashCode() => (GetType() + Id).GetHashCode(StringComparison.OrdinalIgnoreCase);
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+            {
+                return false;
+            }
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b) => !(a == b);
     }
 }
