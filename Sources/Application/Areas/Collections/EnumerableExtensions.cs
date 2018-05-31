b7f45942b2637926b6d851c65bc9c99ed0834eb2
Matthias Mueller <matthiasm@live.de>
2018-05-31 22:35:51 +0200

    WIP

---
+++ Sources/Application/Areas/Collections/EnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mmu.Mlh.LanguageExtensions.Areas.Collections
+{
+    public static class EnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> list, Action<T> action)
+        {
+            foreach (var entry in list)
+            {
+                action(entry);
+            }
+        }
+    }
+}
