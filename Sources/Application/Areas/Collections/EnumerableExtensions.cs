94bbd8468d12cd225b5ada9c99e9996e2999f99a
Matthias Mueller <matthiasm@live.de>
2018-09-10 20:45:29 +0200

    WIP

--- Sources/Application/Areas/Collections/EnumerableExtensions.cs
+++ Sources/Application/Areas/Collections/EnumerableExtensions.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Mmu.Mlh.LanguageExtensions.Areas.Collections
 {
     public static class EnumerableExtensions
     {
+        public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> list, int chunkSize)
+        {
+            return list
+                .Select((x, i) => new { Index = i, Value = x })
+                .GroupBy(x => x.Index / chunkSize)
+                .Select(x => x.Select(v => v.Value).ToList())
+                .ToList();
+        }
+
         public static void ForEach<T>(this IEnumerable<T> list, Action<T> action)
         {
             foreach (var entry in list)
