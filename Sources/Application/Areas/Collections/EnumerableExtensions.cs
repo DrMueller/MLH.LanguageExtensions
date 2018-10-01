d833cf3ee618440e102f8e7cfeb483df80fc7f3b
Matthias Mueller <matthiasm@live.de>
2018-10-01 14:47:54 +0200

    WIP

--- Sources/Application/Areas/Collections/EnumerableExtensions.cs
+++ Sources/Application/Areas/Collections/EnumerableExtensions.cs
@@ -15,6 +15,16 @@ namespace Mmu.Mlh.LanguageExtensions.Areas.Collections
                 .ToList();
         }
 
+        public static bool ContainsAny<T>(this IEnumerable<T> list, IEnumerable<T> otherList)
+        {
+            if (otherList == null)
+            {
+                return false;
+            }
+
+            return list.Intersect(otherList).Any();
+        }
+
         public static void ForEach<T>(this IEnumerable<T> list, Action<T> action)
         {
             foreach (var entry in list)
---
+++ Sources/UnitTests/TestingAreas/Areas/Collections/EnumerableExtensionsUnitTests.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using Mmu.Mlh.LanguageExtensions.Areas.Collections;
+using NUnit.Framework;
+
+namespace Mmu.Mlh.LanguageExtensions.UnitTests.TestingAreas.Areas.Collections
+{
+    [TestFixture]
+    public class EnumerableExtensionsUnitTests
+    {
+        [Test]
+        public void ContainsAny_OtherListBeingNull_ReturnsFalse()
+        {
+            // Arrange
+            var list1 = new List<long>
+            {
+                1,
+                3,
+                5
+            };
+
+            // Act
+            var actualContainsAny = list1.ContainsAny(null);
+
+            // Assert
+            Assert.IsFalse(actualContainsAny);
+        }
+
+        [Test]
+        public void ContainsAny_OtherListContainsEntry_ReturnsTrue()
+        {
+            // Arrange
+            var list1 = new List<long>
+            {
+                1,
+                3,
+                5
+            };
+
+            var list2 = new List<long>
+            {
+                2,
+                4,
+                5
+            };
+
+            // Act
+            var actualContainsAny = list1.ContainsAny(list2);
+
+            // Assert
+            Assert.IsTrue(actualContainsAny);
+        }
+
+        [Test]
+        public void ContainsAny_OtherListDoesNotContainEntry_ReturnsFalse()
+        {
+            // Arrange
+            var list1 = new List<long>
+            {
+                1,
+                3,
+                5
+            };
+
+            var list2 = new List<long>
+            {
+                2,
+                4,
+                6
+            };
+
+            // Act
+            var actualContainsAny = list1.ContainsAny(list2);
+
+            // Assert
+            Assert.IsFalse(actualContainsAny);
+        }
+    }
+}
