ddec55df745fffdcd49b95edc6337f832c148535
Matthias Mueller <matthiasm@live.de>
2018-12-02 21:14:08 +0100

    WIP

---
+++ Sources/Application/Areas/Strings/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Mmu.Mlh.LanguageExtensions.Areas.Strings
+{
+    public static class StringExtensions
+    {
+        public static string AppendIfNotEndingWith(this string str, string toAppend)
+        {
+            if (str.EndsWith(toAppend, StringComparison.Ordinal))
+            {
+                return str;
+            }
+
+            return str + toAppend;
+        }
+    }
+}
