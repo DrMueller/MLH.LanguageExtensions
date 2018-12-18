9941b7041f818d73734aefec28850531fa8e044e
Matthias Mueller <matthiasm@live.de>
2018-12-18 08:47:48 +0100

    WIP

--- Sources/UnitTests/TestingAreas/Areas/DateTimes/UtcDateTimeUnitTests.cs
+++ Sources/UnitTests/TestingAreas/Areas/DateTimes/UtcDateTimeUnitTests.cs
@@ -29,12 +29,8 @@ namespace Mmu.Mlh.LanguageExtensions.UnitTests.TestingAreas.Areas.DateTimes
 
             // Assert
             Assert.IsTrue(actualUtcDateTime.HasValue);
-            Assert.AreNotEqual(localDateTime, actualUtcDateTime.Value);
             Assert.AreNotEqual(localDateTime.Kind, actualUtcDateTime.Value.Kind);
-
-            var expectedTicks = localDateTime.ToUniversalTime().Ticks;
-            var actualTicks = actualUtcDateTime.Value.Ticks;
-            Assert.AreEqual(expectedTicks, actualTicks);
+            Assert.AreEqual(localDateTime.ToUniversalTime().Ticks, actualUtcDateTime.Value.Ticks);
         }
 
         [Test]
