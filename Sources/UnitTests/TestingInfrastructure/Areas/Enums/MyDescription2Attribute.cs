cabb32f068de1f5633c65328dc24c18c69ba1d32
Matthias Mueller <matthiasm@live.de>
2019-01-05 11:27:22 +0100

    WIP

---
+++ Sources/UnitTests/TestingInfrastructure/Areas/Enums/MyDescription2Attribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Mmu.Mlh.LanguageExtensions.UnitTests.TestingInfrastructure.Areas.Enums
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class MyDescription2Attribute : Attribute
+    {
+        public string Test2String { get; }
+
+        public MyDescription2Attribute(string test2String)
+        {
+            Test2String = test2String;
+        }
+    }
+}
---
+++ Sources/UnitTests/TestingInfrastructure/Areas/Enums/TestEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace Mmu.Mlh.LanguageExtensions.UnitTests.TestingInfrastructure.Areas.Enums
+{
+    public enum TestEnum
+    {
+        [MyDescription("TestValue1")]
+        Value1,
+
+        [MyDescription("TestValue2")]
+        Value2 = 1,
+
+        [MyDescription("TestValue3")]
+        Value3 = 2
+    }
+}
