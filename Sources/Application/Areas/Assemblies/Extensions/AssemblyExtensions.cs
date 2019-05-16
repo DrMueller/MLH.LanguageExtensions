82fc2c32bed43f9b7630388bfb2dc014cb19f520
Matthias Müller <MatthiasM@Live.de>
2019-05-16 21:28:45 +0200

    WIP

---
+++ Sources/Application/Areas/Assemblies/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Mmu.Mlh.LanguageExtensions.Areas.Assemblies.Extensions
+{
+    public static class AssemblyExtensions
+    {
+        public static string GetBasePath(this Assembly assembly)
+        {
+            var uri = new UriBuilder(assembly.CodeBase);
+            var result = Uri.UnescapeDataString(uri.Path);
+            result = Path.GetDirectoryName(result);
+
+            return result;
+        }
+    }
+}
---
+++ Sources/UnitTests/TestingAreas/Areas/Assemblies/Extensions/AssemblyExtensionsUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Mmu.Mlh.LanguageExtensions.Areas.Assemblies.Extensions;
+using NUnit.Framework;
+
+namespace Mmu.Mlh.LanguageExtensions.UnitTests.TestingAreas.Areas.Assemblies.Extensions
+{
+    [TestFixture]
+    public class AssemblyExtensionsUnitTests
+    {
+        [Test]
+        public void GettingAssemblyBasePath_GetsValidPath()
+        {
+            // Arrange
+            var thisAssembly = typeof(AssemblyExtensionsUnitTests).Assembly;
+
+            // Act
+            var actualAssemblyPath = thisAssembly.GetBasePath();
+
+            // Assert
+            var isValidUri = Uri.TryCreate(actualAssemblyPath, UriKind.Absolute, out _);
+            Assert.IsTrue(isValidUri);
+        }
+    }
+}
