8b6f5e41778d0e229ba945639db73dbdf834f7e0
Matthias Müller <matthiasm@live.de>
2019-11-14 21:29:10 +0100

    WIP

--- Sources/Application/Areas/Reflection/Extensions/PropertyExtensions.cs
+++ Sources/Application/Areas/Reflection/Extensions/PropertyExtensions.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Linq.Expressions;
 using System.Reflection;
+// ReSharper disable UnusedParameter.Global
 
 namespace Mmu.Mlh.LanguageExtensions.Areas.Reflection.Extensions
 {
