f37e42fcd63f3032752a47ee7c5556b5e702d4a1
Matthias Müller <matthias.mueller@trivadis.com>
2020-03-24 07:13:46 +0100

    WIP

---
+++ Sources/Application/Areas/Types/FunctionsResults/FunctionResultType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mmu.Mlh.LanguageExtensions.Areas.Types.FunctionsResults
+{
+    public enum FunctionResultType
+    {
+        Success,
+        Failure
+    }
+}
