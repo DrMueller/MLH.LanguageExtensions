39e2c3d5c1deb2c4bd2e85aff06589ec6761fa77
Matthias Müller <matthiasm@live.de>
2022-03-16 13:56:35 +0100

    WIP

--- Sources/Application/Areas/Types/FunctionsResults/FunctionResult.cs
+++ Sources/Application/Areas/Types/FunctionsResults/FunctionResult.cs
@@ -2,17 +2,36 @@
 
 namespace Mmu.Mlh.LanguageExtensions.Areas.Types.FunctionsResults
 {
-    public static class FunctionResult
+    public class FunctionResult
     {
+        public FunctionResult(bool isSuccess)
+        {
+            IsSuccess = isSuccess;
+        }
+
+        public bool IsSuccess { get; }
+
+        public static FunctionResult CreateFailure()
+        {
+            return new FunctionResult(false);
+        }
+
         public static FunctionResult<T> CreateFailure<T>()
         {
-            return new FunctionResult<T>(false, default!);
+#pragma warning disable CS8604 // Possible null reference argument.
+            return new FunctionResult<T>(false, default);
+#pragma warning restore CS8604 // Possible null reference argument.
         }
 
         public static FunctionResult<T> CreateFromDefault<T>(T value)
         {
             // Careful, default doesn't work, it has to be default(T)
             return Equals(value, default(T)) ? CreateFailure<T>() : CreateSuccess(value);
+        }
+
+        public static FunctionResult CreateSuccess()
+        {
+            return new FunctionResult(true);
         }
 
         public static FunctionResult<T> CreateSuccess<T>(T value)
@@ -26,15 +45,14 @@ namespace Mmu.Mlh.LanguageExtensions.Areas.Types.FunctionsResults
         "SA1402:FileMayOnlyContainASingleType",
         Justification =
             "It makes sense to keep these Classes together")]
-    public class FunctionResult<T>
+    public class FunctionResult<T> : FunctionResult
     {
         public FunctionResult(bool isSuccess, T value)
+            : base(isSuccess)
         {
-            IsSuccess = isSuccess;
             Value = value;
         }
 
-        public bool IsSuccess { get; }
         public T Value { get; }
     }
 }
