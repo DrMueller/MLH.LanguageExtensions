4b58c11c912b84d4feaf0a0800e33992361c640d
Matthias Müller <matthias.mueller@mobi.ch>
2022-11-12 14:56:51 +0100

    WIP

---
+++ Sources/Application/Areas/Tasks/TaskExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mmu.Mlh.LanguageExtensions.Areas.Tasks
+{
+    public static class TaskExtensions
+    {
+        public static async Task<IReadOnlyCollection<TResult>> SelectAsync<T, TResult>(
+            this Task<IReadOnlyCollection<T>> task,
+            Func<T, TResult> selector)
+        {
+#pragma warning disable VSTHRD003 // Avoid awaiting foreign Tasks
+            var data = await task;
+#pragma warning restore VSTHRD003 // Avoid awaiting foreign Tasks
+
+            return data
+                .Select(selector)
+                .ToList();
+        }
+    }
+}
---
+++ Sources/UnitTests/TestingAreas/Areas/Tasks/TaskExtensionsUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Mmu.Mlh.LanguageExtensions.Areas.Tasks;
+using Xunit;
+
+namespace Mmu.Mlh.LanguageExtensions.UnitTests.TestingAreas.Areas.Tasks
+{
+    public class TaskExtensionsUnitTests
+    {
+        [Fact]
+        public async Task SelectAsync_SelectsAsync()
+        {
+            // Arrange
+            var list = new List<string>
+            {
+                "Test1",
+                "Test2",
+                "Test23"
+            };
+
+            var selectTask = Task.Run<IReadOnlyCollection<string>>(() => list);
+
+            // Act
+            var result = await selectTask.SelectAsync(f => f);
+
+            // Assert
+            result.Should().BeEquivalentTo(list);
+        }
+    }
+}
