9723c03d7e9d9fc6b0de32506f0195310afaa40b
Müller Matthias <matthias.mueller@noser.com>
2024-03-20 14:48:33 +0100

    WIP

---
+++ Sources/Application/Areas/Maths/DoubleExtensions.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mmu.Mlh.LanguageExtensions.Areas.Maths
+{
+    public static class DoubleExtensions
+    {
+        public static double Add(this double value, double valueToADd)
+        {
+            var mydecimal1 = new decimal(value);
+            var mydecimal2 = new decimal(valueToADd);
+            var res = decimal.Add(mydecimal1, mydecimal2);
+
+            return decimal.ToDouble(res);
+        }
+
+        public static double Add(this double? value, double? valueToAdd)
+        {
+            var mydecimal1 = new decimal(value ?? 0.0);
+            var mydecimal2 = new decimal(valueToAdd ?? 0.0);
+            var res = decimal.Add(mydecimal1, mydecimal2);
+
+            return decimal.ToDouble(res);
+        }
+
+        public static double Aggregate(this List<double?> list)
+        {
+            var res = new decimal(0);
+
+            foreach (var myDouble in list)
+            {
+                var mydecimal2 = new decimal(myDouble ?? 0.0);
+                res = decimal.Add(res, mydecimal2);
+            }
+
+            return decimal.ToDouble(res);
+        }
+
+        public static double Divide(this double value, double valueToDivide)
+        {
+            var mydecimal1 = new decimal(value);
+            var mydecimal2 = new decimal(valueToDivide);
+
+            return decimal.ToDouble(Divide(mydecimal1, mydecimal2));
+        }
+
+        public static double Divide(this double? value, double? value2)
+        {
+            var mydecimal1 = new decimal(value ?? 0.0);
+            var mydecimal2 = new decimal(value2 ?? 0.0);
+
+            return decimal.ToDouble(Divide(mydecimal1, mydecimal2));
+        }
+
+        public static bool IsEqual(this double value, double value2)
+        {
+            var mydecimal1 = new decimal(value);
+            var mydecimal2 = new decimal(value2);
+
+            return mydecimal1 == mydecimal2;
+        }
+
+        public static bool IsEqual(this double? value, double? value2)
+        {
+            var mydecimal1 = new decimal(value ?? 0.0);
+            var mydecimal2 = new decimal(value2 ?? 0.0);
+
+            return mydecimal1 == mydecimal2;
+        }
+
+        public static double Multiply(this double value, double value2)
+        {
+            var mydecimal1 = new decimal(value);
+            var mydecimal2 = new decimal(value2);
+            var res = decimal.Multiply(mydecimal1, mydecimal2);
+
+            return decimal.ToDouble(res);
+        }
+
+        public static double Multiply(this double? value, double? value2)
+        {
+            var mydecimal1 = new decimal(value ?? 0.0);
+            var mydecimal2 = new decimal(value2 ?? 0.0);
+            var res = decimal.Multiply(mydecimal1, mydecimal2);
+
+            return decimal.ToDouble(res);
+        }
+
+        public static double RoundWith0Digits(this double value)
+        {
+            return RoundWithDigits(value, 0);
+        }
+
+        public static double RoundWith2Digits(this double value)
+        {
+            return RoundWithDigits(value, 2);
+        }
+
+        public static double RoundWith3Digits(this double value)
+        {
+            return RoundWithDigits(value, 3);
+        }
+
+        public static double Subtract(this double value, double value2)
+        {
+            var mydecimal1 = new decimal(value);
+            var mydecimal2 = new decimal(value2);
+            var res = decimal.Subtract(mydecimal1, mydecimal2);
+
+            return decimal.ToDouble(res);
+        }
+
+        public static double Subtract(this double? value, double? value2)
+        {
+            var mydecimal1 = new decimal(value ?? 0.0);
+            var mydecimal2 = new decimal(value2 ?? 0.0);
+            var res = decimal.Subtract(mydecimal1, mydecimal2);
+
+            return decimal.ToDouble(res);
+        }
+
+        private static decimal Divide(decimal value1, decimal value2)
+        {
+            if (value2.Equals(0))
+            {
+                return new decimal(0);
+            }
+
+            return decimal.Divide(value1, value2);
+        }
+
+        private static double RoundWithDigits(double value, int digits)
+        {
+            if (double.IsNaN(value))
+            {
+                return 0.0;
+            }
+
+            var unroundedDecimal = new decimal(value);
+            var roundedDecimal = Math.Round(unroundedDecimal, digits, MidpointRounding.AwayFromZero);
+
+            return decimal.ToDouble(roundedDecimal);
+        }
+    }
+}
---
+++ Sources/UnitTests/TestingAreas/Areas/Maths/DoubleExtensionsUnitTests.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Mmu.Mlh.LanguageExtensions.Areas.Maths;
+using Xunit;
+
+namespace Mmu.Mlh.LanguageExtensions.UnitTests.TestingAreas.Areas.Maths
+{
+    public class DoubleExtensionsUnitTests
+    {
+        [Fact]
+        public void AddingDoubles_AddsDoubles()
+        {
+            // Arrange
+            var val1 = 0.1;
+            var val2 = 0.2;
+
+            // Act
+            var actualVal = val1.Add(val2);
+            var nativeVal = val1 + val2;
+
+            // Assert
+            actualVal.Should().NotBe(nativeVal);
+            actualVal.Should().Be(0.3);
+        }
+
+        [Fact]
+        public void AddingNullableDoubles_ValueBeingNull_DoesntAddValue()
+        {
+            // Arrange
+            double? val1 = 86.25;
+
+            // Act
+            var actualVal = val1.Add(null);
+
+            // Assert
+            actualVal.Should().Be(val1);
+        }
+
+        [Fact]
+        public void Aggregating_AggregatesValues()
+        {
+            // Arrange
+            var vals = new List<double?>
+            {
+                5.555,
+                4.333,
+                null
+            };
+
+            // Act
+            var actualVal = vals.Aggregate();
+
+            // Assert
+            actualVal.Should().Be(9.888);
+        }
+
+        [Fact]
+        public void DividingDoubles_DividesDoubles()
+        {
+            // Arrange
+            var val1 = 0.3;
+            var val2 = 0.1;
+
+            // Act
+            var actualVal = val1.Divide(val2);
+            var nativeVal = val1 / val2;
+
+            // Assert
+            actualVal.Should().NotBe(nativeVal);
+            actualVal.Should().Be(3);
+        }
+
+        [Fact]
+        public void DividingNullableDoubles_ValueBeingNull_Returns0()
+        {
+            // Arrange
+            double? val1 = 86.25;
+
+            // Act
+            var actualVal = val1.Divide(null);
+
+            // Assert
+            actualVal.Should().Be(0);
+        }
+
+        [Fact]
+        public void MultiplyingDoubles_MultipliesDoubles()
+        {
+            // Arrange
+            var val1 = 0.1;
+            var val2 = 3;
+
+            // Act
+            var actualVal = val1.Multiply(val2);
+            var nativeVal = val1 * val2;
+
+            // Assert
+            actualVal.Should().NotBe(nativeVal);
+            actualVal.Should().Be(0.3);
+        }
+
+        [Fact]
+        public void MultiplyingNullableDoubles_ValueBeingNull_Returns0()
+        {
+            // Arrange
+            double? val1 = 0.1;
+
+            // Act
+            var actualVal = val1.Multiply(null);
+
+            // Assert
+            actualVal.Should().Be(0);
+        }
+
+        [Fact]
+        public void RoundingWith3Digits_RoundsDown()
+        {
+            // Arrange
+            var number = 2.5554;
+
+            // Act
+            var result = number.RoundWith3Digits();
+
+            // Assert
+            result.Should().Be(2.555);
+        }
+
+        [Fact]
+        public void RoundingWith3Digits_RoundsUp()
+        {
+            // Arrange
+            var number = 2.5555;
+
+            // Act
+            var result = number.RoundWith3Digits();
+
+            // Assert
+            result.Should().Be(2.556);
+        }
+
+        [Fact]
+        public void SubtractingDoubles_SubtractsDoubles()
+        {
+            // Arrange
+            var val1 = 86.25;
+            var val2 = 86.24;
+
+            // Act
+            var actualVal = val1.Subtract(val2);
+            var nativeVal = val1 - val2;
+
+            // Assert
+            actualVal.Should().NotBe(nativeVal);
+            actualVal.Should().Be(0.01);
+        }
+
+        [Fact]
+        public void SubtractingNullableDoubles_ValueBeingNull_DoesntSubtractValue()
+        {
+            // Arrange
+            double? val1 = 86.25;
+
+            // Act
+            var actualVal = val1.Subtract(null);
+
+            // Assert
+            actualVal.Should().Be(val1);
+        }
+    }
+}
